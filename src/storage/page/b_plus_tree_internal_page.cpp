3cfeec2c1700f8567c803056e68cba06c7ebc45e
zmk-wawa <80107411+zmk-wawa@users.noreply.github.com>
2024-04-05 21:10:12 +0800

    Pj0-1

--- src/storage/page/b_plus_tree_internal_page.cpp
+++ src/storage/page/b_plus_tree_internal_page.cpp
@@ -76,9 +76,9 @@ auto B_PLUS_TREE_INTERNAL_PAGE_TYPE::ValueIndex(const ValueType &value) const ->
  * 因为在这个B+树的设计中，内部节点的value存放的子树列表是前一个的范围  前一个k≤ 子树 < 其k
  * 我们需要的就是这个子树的页id的首节点
  * 这里要注意的是 第一个位置不存放k  即n个value只需要n-1个key
- * key:      空   |  key1  |  key2  | key3 |
- * value:           ＜key1 |k1≤ ＜k2| k2≤ ＜k3| ≥k3
- * 获取方式:           key1   key2     key3     array_[GetSize()-1].second()
+ * key:      空   |  key1  |  key2  |  key3  | ... | array_GetSize()-1.first()
+ * value:  ＜key1 |k1≤ ＜k2| k2≤ ＜k3| k3≤ ≤k4| ... | ≥ array_GetSize()-1.first()
+ * KV对: <null,V0>| <K1,V1>| <K2,V2>| <K3,V3>| ... | ...
  * */
 INDEX_TEMPLATE_ARGUMENTS
 auto B_PLUS_TREE_INTERNAL_PAGE_TYPE::Lookup(const KeyType &key, const KeyComparator &comparator) const -> ValueType {
@@ -127,18 +127,27 @@ auto B_PLUS_TREE_INTERNAL_PAGE_TYPE::InsertNodeAfter(const ValueType &oldValue, 
   return GetSize();
 }
 
+/*
+ * 将后半段分离出去，加到另一个页中
+ * 用于长度超过MaxSize时
+ * recipient：需要加进的目的页
+ * */
 INDEX_TEMPLATE_ARGUMENTS
 void B_PLUS_TREE_INTERNAL_PAGE_TYPE::MoveHalfTo(BPlusTreeInternalPage<KeyType,ValueType, KeyComparator> *recipient, BufferPoolManager *bufferPoolManager) {
   int splitFromIndex = GetMinSize();
-  int
+  int splitSize = GetSize() - splitFromIndex;
+  SetSize(splitFromIndex); // 设置分离后，原页中的size
+  recipient->CopyNFrom(array_+ splitFromIndex, splitSize,bufferPoolManager);
 }
 
 /*
  * 一般用于将某一段(可能是长度不够MinSize 可能是长度超过MaxSize)复制到另一个internal的页中
+ * 某段： items~items+size-1
  * 1. 将给定的某一段复制到当前internal页(待合入的目的页)的array_中
  * 2. 某一段中的每个KV对中的value都是这个KV对中的K指向的其下一层的子树列表的pageId
  *    也就是说，每一个value对应的页，其parent page id仍是这个KV对所在的源page id
  *    我们已经将这个KV对迁移过来了，还不要忘记更新V对应的页的Parent page id为当前internal页 id
+ * 3. 更新目的页中含有的KV对数目
  * 注意：我们更新页是在Buffer pool上更新，更新后置为脏页，不立即写回磁盘捏！
  * */
 INDEX_TEMPLATE_ARGUMENTS
@@ -154,9 +163,94 @@ void B_PLUS_TREE_INTERNAL_PAGE_TYPE::CopyNFrom(std::pair<KeyType, ValueType> *it
     // 解除固定，告诉Buffer pool现在是脏页(不立即写回)
     bufferPoolManager->UnpinPage(page->GetPageId(),true);
   }
+  IncreaseSize(size);
 }
 
+/*
+ * 删除页KV对列表中，索引为index位置的KV对
+ * */
+INDEX_TEMPLATE_ARGUMENTS
+void B_PLUS_TREE_INTERNAL_PAGE_TYPE::Remove(int index) {
+  std::move(array_+index+1,array_ + GetSize(),array_+index);
+  IncreaseSize(-1);
+}
 
+INDEX_TEMPLATE_ARGUMENTS
+auto B_PLUS_TREE_INTERNAL_PAGE_TYPE::RemoveAndReturnOnlyChild() -> ValueType {
+  ValueType onlyValue = ValueAt(0);
+  SetSize(0);
+  return onlyValue;
+}
+
+/*
+ * 将此时页中的所有KV对移到另一个页中
+ * 用于size < MinSize 时，合并入其他页
+ * 将自己整个迁移过去，而原本自己的第一个KV对是<null,V0>，所以
+ * 1. 要将自己第一个K0补上，也就是应该是目标页的最后一个K(如果不超MaxSize)；
+ * 2. 最后迁移过去后，
+ * 3. 不要忘记将源页size设置为0
+ * recipient：其他页  middleKey：目标页的最后一个K
+ * */
+INDEX_TEMPLATE_ARGUMENTS
+void B_PLUS_TREE_INTERNAL_PAGE_TYPE::MoveAllTo(BPlusTreeInternalPage<KeyType, ValueType, KeyComparator> *recipient, const KeyType &middleKey, BufferPoolManager *bufferPoolManager) {
+  SetKeyAt(0,middleKey);
+  recipient->CopyNFrom(array_,GetSize(),bufferPoolManager);
+  SetSize(0);
+}
+
+/*
+ * 将第一个KV对(其中K用middleKey赋值)移到另一个页后
+ * */
+INDEX_TEMPLATE_ARGUMENTS
+void B_PLUS_TREE_INTERNAL_PAGE_TYPE::MoveFirstToEndOf(BPlusTreeInternalPage<KeyType, ValueType, KeyComparator> *recipient, const KeyType &middleKey, BufferPoolManager *bufferPoolManager) {
+  SetKeyAt(0,middleKey);
+  auto sourceFirstPair = array_[0];
+  recipient->CopyLastFrom(sourceFirstPair,bufferPoolManager);
+
+  std::move(array_+1,array_+GetSize(),array_); // 删除KV对中的第一个(删除数组第一个)
+  IncreaseSize(-1);
+}
+
+/*
+ * 将给定的一个pair添加到当前页最后(步骤同CopyNFrom)
+ * */
+INDEX_TEMPLATE_ARGUMENTS
+void B_PLUS_TREE_INTERNAL_PAGE_TYPE::CopyLastFrom(const std::pair<KeyType, ValueType> &pair, BufferPoolManager *bufferPoolManager) {
+  *(array_ + GetSize()) = pair;
+  IncreaseSize(1);
+
+  auto page = bufferPoolManager->FetchPage(pair.second);
+  auto *node = reinterpret_cast<BPlusTreePage *>(page->GetData());
+  node->SetParentPageId(GetPageId());
+  bufferPoolManager->UnpinPage(page->GetPageId(),true);
+}
+
+/*
+ * 将最后一个KV对(其中K用middleKey赋值)移到另一个页的KV list最开头
+ * */
+INDEX_TEMPLATE_ARGUMENTS
+void B_PLUS_TREE_INTERNAL_PAGE_TYPE::MoveLastToFrontOf(BPlusTreeInternalPage<KeyType, ValueType, KeyComparator> *recipient, const KeyType &middleKey, BufferPoolManager *bufferPoolManager) {
+  auto lastItem = array_[GetSize() - 1];
+  recipient->SetKeyAt(0,middleKey);
+  recipient->CopyFirstFrom(lastItem,bufferPoolManager);
+
+  IncreaseSize(-1);
+}
+
+/*
+ * 将pari插入页的KV对的开头
+ * */
+INDEX_TEMPLATE_ARGUMENTS
+void B_PLUS_TREE_INTERNAL_PAGE_TYPE::CopyFirstFrom(const std::pair<KeyType, ValueType> &pair, BufferPoolManager *bufferPoolManager) {
+  std::move_backward(array_,array_+GetSize(),array_+GetSize()+1);
+  *array_ = pair;
+  IncreaseSize(1);
+
+  auto page = bufferPoolManager->FetchPage(pair.second);
+  auto *node = reinterpret_cast<BPlusTreePage *>(page->GetData());
+  node->SetParentPageId(GetPageId());
+  bufferPoolManager->UnpinPage(page->GetPageId(),true);
+}
 
 // valuetype for internalNode should be page id_t
 template class BPlusTreeInternalPage<GenericKey<4>, page_id_t, GenericComparator<4>>;
--- src/storage/page/b_plus_tree_leaf_page.cpp
+++ src/storage/page/b_plus_tree_leaf_page.cpp
@@ -26,8 +26,21 @@ namespace bustub {
  * Including set page type, set current size to zero, set page id/parent id, set
  * next page id and set max size
  */
+/*
+ * 与internal不同的是，这里K与V是一一对应的，第一个Key也不为空
+ * 多了一个在本页KV对最后，指向下一个叶list(页)的值:next page id,初始设置为INVALID_PAGE_ID
+ * 如果向后加入新的，则next page id应该改为新的指向的id
+ * */
 INDEX_TEMPLATE_ARGUMENTS
-void B_PLUS_TREE_LEAF_PAGE_TYPE::Init(page_id_t page_id, page_id_t parent_id, int max_size) {}
+void B_PLUS_TREE_LEAF_PAGE_TYPE::Init(page_id_t page_id, page_id_t parent_id, int max_size) {
+    SetPageType(IndexPageType::LEAF_PAGE);
+    SetSize(0);
+    SetPageId(page_id);
+    SetParentPageId(parent_id);
+    SetMaxSize(max_size);
+    // 与internal页相比的区别，叶的value就是这个key所代表的数据的内存地址！
+    SetNextPageId(INVALID_PAGE_ID);
+}
 
 /**
  * Helper methods to set/get next page id
@@ -36,7 +49,7 @@ INDEX_TEMPLATE_ARGUMENTS
 auto B_PLUS_TREE_LEAF_PAGE_TYPE::GetNextPageId() const -> page_id_t { return INVALID_PAGE_ID; }
 
 INDEX_TEMPLATE_ARGUMENTS
-void B_PLUS_TREE_LEAF_PAGE_TYPE::SetNextPageId(page_id_t next_page_id) {}
+void B_PLUS_TREE_LEAF_PAGE_TYPE::SetNextPageId(page_id_t next_page_id) {next_page_id_ = next_page_id;}
 
 /*
  * Helper method to find and return the key associated with input "index"(a.k.a
@@ -44,9 +57,139 @@ void B_PLUS_TREE_LEAF_PAGE_TYPE::SetNextPageId(page_id_t next_page_id) {}
  */
 INDEX_TEMPLATE_ARGUMENTS
 auto B_PLUS_TREE_LEAF_PAGE_TYPE::KeyAt(int index) const -> KeyType {
-  // replace with your own code
-  KeyType key{};
-  return key;
+    return array_[index].first;
+}
+
+INDEX_TEMPLATE_ARGUMENTS
+auto B_PLUS_TREE_LEAF_PAGE_TYPE::GetItem(int index) -> const std::pair<KeyType, ValueType> & {
+    return array_[index];
+}
+
+/*
+ * 用于插入或者查找是否在页中，定位应在的index
+ * 类似internal页中的Lookup, 找到这个key所在的KV对的index，或者是找到这个要插入的key应该在的index
+ * 区别在于这个不存在区间，要找到第一个不小于key的位置的index( ==key，找到；> key 要插入)
+ * */
+INDEX_TEMPLATE_ARGUMENTS
+auto B_PLUS_TREE_LEAF_PAGE_TYPE::KeyIndex(const KeyType &key, const KeyComparator &comparator) const -> int {
+    auto item = std::lower_bound(array_,array_+ GetSize(),key,[&comparator](const auto &pair,auto k){
+      return comparator(pair.first, k) < 0;
+    });
+    // 此时就是item.first == key的位置
+    return std::distance(array_,item);
+}
+
+/*
+ * 插入叶<key,value>与页中
+ * 1. key存在，返回
+ * 2. 要插入尾端、
+ * 3. 要插入中间
+ * */
+INDEX_TEMPLATE_ARGUMENTS
+  auto B_PLUS_TREE_LEAF_PAGE_TYPE::Insert(const KeyType &key, const ValueType &value, const KeyComparator &keyComparator) -> int {
+    auto index = KeyIndex(key,keyComparator);
+    // 2.
+    if(index == GetSize()){
+      *(array_+index) = {key,value};
+      IncreaseSize(1);
+      return GetSize();
+    }
+    // 1.
+    if(keyComparator(array_[index].first,key) == 0){
+      return GetSize();
+    }
+    // 3.
+    std::move_backward(array_+index, array_ +GetSize(),array_+GetSize()+1);
+    *(array_+index) = {key,value};
+    IncreaseSize(1);
+    return GetSize();
+  }
+
+  /*
+   * 用于要超过MaxSize，split，方法同internal页
+   * */
+INDEX_TEMPLATE_ARGUMENTS
+  void B_PLUS_TREE_LEAF_PAGE_TYPE::MoveHalfTo(BPlusTreeLeafPage<KeyType, ValueType, KeyComparator> *recipient) {
+  int splitStartIndex = GetMinSize();
+  int size = GetSize() - splitStartIndex;
+  SetSize(splitStartIndex);
+  recipient->CopyNFrom(array_+splitStartIndex,size);
+}
+
+/*
+ * 不存在有KV对的key为空，所以可以直接copy
+ * */
+INDEX_TEMPLATE_ARGUMENTS
+void B_PLUS_TREE_LEAF_PAGE_TYPE::CopyNFrom(std::pair<KeyType, ValueType> *items, int size) {
+    std::copy(items,items+size,array_+GetSize());
+    IncreaseSize(size);
+}
+
+/*
+ * 能否查到key为key的节点，返回值是布尔值
+ * */
+INDEX_TEMPLATE_ARGUMENTS
+auto B_PLUS_TREE_LEAF_PAGE_TYPE::Lookup(const KeyType &key, ValueType *value, const KeyComparator &keyComparator) const -> bool {
+    int index = KeyIndex(key,keyComparator);
+    if(index == GetSize() || keyComparator(array_[index].first,key) != 0){
+      return false;
+    }
+    return true;
+}
+
+/*
+ * 删除并返回删除后的KV对数量
+ * */
+INDEX_TEMPLATE_ARGUMENTS
+auto B_PLUS_TREE_LEAF_PAGE_TYPE::RemoveAndDeleteRecord(const KeyType &key, const KeyComparator &keyComparator) -> int {
+    int index = KeyIndex(key,keyComparator);
+    if(index == GetSize() || keyComparator(array_[index].first,key) != 0){
+      return GetSize();
+    }
+    std::move(array_+index+1,array_+GetSize(),array_+index);
+    IncreaseSize(-1);
+    return GetSize();
+}
+
+/*
+ * 将本页的KV对全部移到其他页里面
+ * -> 与internal不同的是，这里K与V是一一对应的，第一个Key也不为空
+ * -> 多了一个指向下一个叶list(页)的值:next page id
+ * 由于是本页的KV加到其他页后，所以其他页的next_page_id要更新为本页的！
+ * */
+INDEX_TEMPLATE_ARGUMENTS
+void B_PLUS_TREE_LEAF_PAGE_TYPE::MoveAllTo(BPlusTreeLeafPage<KeyType, ValueType, KeyComparator> *recipient) {
+    recipient->CopyNFrom(array_,GetSize());
+    recipient->SetNextPageId(GetNextPageId());
+    SetSize(0);
+}
+
+INDEX_TEMPLATE_ARGUMENTS
+void B_PLUS_TREE_LEAF_PAGE_TYPE::MoveFirstToEndOf(BPlusTreeLeafPage<KeyType, ValueType, KeyComparator> *recipient) {
+  auto sourceFirstItem = GetItem(0);
+  std::move(array_+1,array_+GetSize(),array_);
+  IncreaseSize(-1);
+  recipient->CopyLastFrom(sourceFirstItem);
+}
+
+INDEX_TEMPLATE_ARGUMENTS
+void B_PLUS_TREE_LEAF_PAGE_TYPE::CopyLastFrom(const std::pair<KeyType, ValueType> &item) {
+  *(array_ + GetSize()) = item;
+  IncreaseSize(1);
+}
+
+INDEX_TEMPLATE_ARGUMENTS
+void B_PLUS_TREE_LEAF_PAGE_TYPE::MoveLastToFrontOf(BPlusTreeLeafPage<KeyType, ValueType, KeyComparator> *recipient) {
+  auto sourceLastItem = array_[GetSize()-1];
+  IncreaseSize(-1);
+  recipient->CopyFirstFrom(sourceLastItem);
+}
+
+INDEX_TEMPLATE_ARGUMENTS
+void B_PLUS_TREE_LEAF_PAGE_TYPE::CopyFirstFrom(const std::pair<KeyType, ValueType> &item) {
+  std::move_backward(array_,array_+ GetSize(),array_+GetSize()+1);
+  *array_ = item;
+  IncreaseSize(1);
 }
 
 template class BPlusTreeLeafPage<GenericKey<4>, RID, GenericComparator<4>>;
