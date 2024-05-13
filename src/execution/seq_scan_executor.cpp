5766b914bcc5e855c0a8302484344989bd5578f2
ygzhang-yolo <1018763328@qq.com>
2024-05-13 10:30:16 +0000

    feat: task 1: seq_scan, insert, delete completed!

--- src/execution/seq_scan_executor.cpp
+++ src/execution/seq_scan_executor.cpp
@@ -14,10 +14,32 @@
 
 namespace bustub {
 
-SeqScanExecutor::SeqScanExecutor(ExecutorContext *exec_ctx, const SeqScanPlanNode *plan) : AbstractExecutor(exec_ctx) {}
+SeqScanExecutor::SeqScanExecutor(ExecutorContext *exec_ctx, const SeqScanPlanNode *plan)
+    : AbstractExecutor(exec_ctx), plan_(plan) {
+  this->table_info_ = this->exec_ctx_->GetCatalog()->GetTable(plan_->table_oid_);
+}
 
-void SeqScanExecutor::Init() { throw NotImplementedException("SeqScanExecutor is not implemented"); }
-
-auto SeqScanExecutor::Next(Tuple *tuple, RID *rid) -> bool { return false; }
+/*
+ * Init: 获取TableIterator的首个tuple
+*/
+void SeqScanExecutor::Init() {
+  // 让table_iter从当前事务开始
+  this->table_iter_ = table_info_->table_->Begin(exec_ctx_->GetTransaction());
+}
+/*
+ * Next: 遍历返回每一个匹配的tuple和他的原始RID;
+*/
+auto SeqScanExecutor::Next(Tuple *tuple, RID *rid) -> bool {
+  do {
+    if (table_iter_ == table_info_->table_->End()) {
+      return false;
+    }
+    *tuple = *table_iter_;
+    *rid = tuple->GetRid();
+    ++table_iter_;
+  } while (plan_->filter_predicate_ != nullptr &&
+           !plan_->filter_predicate_->Evaluate(tuple, table_info_->schema_).GetAs<bool>());
+  return true;
+}
 
 }  // namespace bustub
--- src/include/execution/executors/seq_scan_executor.h
+++ src/include/execution/executors/seq_scan_executor.h
@@ -14,9 +14,12 @@
 
 #include <vector>
 
+#include "catalog/catalog.h"
+#include "common/rid.h"
 #include "execution/executor_context.h"
 #include "execution/executors/abstract_executor.h"
 #include "execution/plans/seq_scan_plan.h"
+#include "storage/table/table_iterator.h"
 #include "storage/table/tuple.h"
 
 namespace bustub {
@@ -50,5 +53,9 @@ class SeqScanExecutor : public AbstractExecutor {
  private:
   /** The sequential scan plan node to be executed */
   const SeqScanPlanNode *plan_;
+
+  // 需要增加的成员变量
+  TableIterator table_iter_ = {nullptr, RID(), nullptr};
+  const TableInfo *table_info_;
 };
 }  // namespace bustub
